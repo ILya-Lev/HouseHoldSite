be9f9d11a4fc62d29f4f36625052ed204494e48b
ILya-Lev <lis2889@gmail.com>
2017-04-17 23:36:27 +0300

    Add project files.

---
+++ HouseHoldSite/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(HouseHoldSite.Startup))]
+namespace HouseHoldSite
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
